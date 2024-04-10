70644028ff26be8f1703a07ef5b520831981edd0
dhruv-8901 <pateldhruv123890@gmail.com>
2024-04-10 22:30:42 +0530

    join group

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const http = require("http").Server(app);
 
 ioConnection.attach(http);
 
+app.set("view engine", "ejs");
+app.set("views", path.join(`${__dirname}/`, "views"));
+
 app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  return res.render("join");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.group);
+  if (req.body.name && req.body.group) {
+    return res.render("chat", {
+      name: req.body.name,
+      groupId: req.body.group,
+    });
+  }
+  return res.render("join");
 });
 
 http.listen(6003, () => {
